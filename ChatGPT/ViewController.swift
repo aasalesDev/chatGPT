33d5ba8951ca894c6ad23e46041295c530e2f1ba
Anderson Sales <andersonsales.mg@gmail.com>
2023-04-03 02:40:32 -0300

    Create ChatGPT Project

---
+++ ChatGPT/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ChatGPT
+//
+//  Created by Anderson Sales on 03/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+}
+
